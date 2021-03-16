e4f2d30644481ab6b38c6d076db07896620ccc17
Luis de la Cruz <gael130819@gmail.com>
2021-03-16 17:48:37 -0500

    fix

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,50 +1,9 @@
 import Vue from 'vue'
-import Vuetify, {
-    VApp,
-    VToolbar,
-    VCard,
-    VBtn,
-    VDialog,
-    VDivider,
-    VGrid,
-    VIcon,
-    VProgressCircular,
-    VList,
-    VAvatar,
-    VListGroup,
-  } from 'vuetify/lib';
+import Vuetify from 'vuetify/lib'
 import 'vuetify/dist/vuetify.min.css';
 
-Vue.use(Vuetify, {
-    components: {
-      VApp,
-      VToolbar,
-      VCard,
-      VBtn,
-      VProgressCircular,
-      VList,
-      VListGroup,
-      VAvatar,
-      VIcon,
-      VDialog,
-      VDivider,
-      VGrid
-    },
-})
+Vue.use(Vuetify)
 
-export default new Vuetify({
-  // icons: {
-  //   iconfont: 'md', // 'mdi' || 'mdiSvg' || 'md' || 'fa' || 'fa4'
-  // },
-  theme: {
-    themes: {
-      light: {
-        primary: '#24A4ED',
-        error: "#FA3F25",
-        warning: "#FFA800",
-        success: "#2AAF00",
-        black: "#333",
-      },
-    },
-  },
-});
+const opts = {}
+
+export default new Vuetify(opts)
--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,5 @@
 module.exports = {
-  /*transpileDependencies: [
+  transpileDependencies: [
     'vuetify'
-  ],*/
-  configureWebpack:{
-    performance: {
-      hints: false
-    },
-    optimization: {
-      splitChunks: {
-        minSize: 10000,
-        maxSize: 250000,
-      }
-    },
-  }
+  ]
 }
